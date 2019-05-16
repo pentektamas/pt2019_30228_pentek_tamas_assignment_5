3ae3428d0f3a798b720761855aaa54787a8e84c8
Pentek Tamas <pentek_pityu@yahoo.com>
2019-05-16 11:39:29 +0300

    initial commit

---
+++ AnalyzingTask/src/main/java/Lambda/AnalyzingTask/App.java
@@ -0,0 +1,13 @@
+package Lambda.AnalyzingTask;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
